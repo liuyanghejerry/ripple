57e12b8f2a0a07804dcd718b81bac2c00d904869
liuyanghejerry <liuyanghejerry@126.com>
2015-01-01 16:25:54 +0800

    init

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+(function() {
+  $(document).ready(function(){
+    attachBackground();
+  });
+
+  function attachBackground() {
+    var items = $('.background-slider .background').each(function(index, item) {
+      var item = $(item);
+      var imgUrl = item.data('background');
+      imgUrl = "url('"+imgUrl+"') center center no-repeat";
+      item.css('background', imgUrl);
+      item.on('click', function(){
+        console.log('click', index);
+        next();
+      });
+    });
+    $(items[items.length-1]).addClass('active').show();
+    function next() {
+      var active = items.siblings('.active');
+      active.prev().show().addClass('active');
+      active.fadeOut(800, function(){
+        active.removeClass('active').prependTo(active.parent());
+      });
+    }
+  }
+})();
