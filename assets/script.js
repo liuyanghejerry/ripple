75cc1fee9e5a4441c817460bc634ad68876a0f62
liuyanghejerry <liuyanghejerry@126.com>
2015-01-02 21:28:52 +0800

    修改缩放逻辑（未完）

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,34 @@
 
   function attachBackground() {
     var background = $('.background-slider .background').first();
-    var images = $('.background-slider .background img').map(function(index,item){return $(item).prop('src')})
+    var images = $('.background-slider .background img').map(function(index,item){return $(item).prop('src')});
+
+    $(window).resize(function() {
+      resize();
+      console.log(background.css('background-size'));
+    });
+
+    function resize() {
+      var realImages = $('.background-slider .background img');
+      var realImage = $(realImages[background.data('background')]);
+
+      var imageWidth = realImage.width();
+      var imageHeight = realImage.height();
+      var windowWidth = $(window).width();
+      var windowHeight = $(window).height();
+      var maxWidth = Math.max(imageWidth, windowWidth);
+      var maxHeight = Math.max(imageHeight, windowHeight);
+
+      var ratio = imageHeight / imageWidth;
+      var isImageHeightLargeThanWidth = ratio > 1;
+      var isWindowHeightLargeThanWidth = windowHeight > windowWidth;
+      var length = isWindowHeightLargeThanWidth ? windowWidth : windowHeight;
+
+      var width = Math.max(isImageHeightLargeThanWidth ? length : length/ratio, windowWidth);
+      var height = Math.max(isImageHeightLargeThanWidth ? length*ratio : length, windowHeight);
+
+      background.css('background-size', '' + (width) + 'px ' + (height) + 'px');
+    }
     
     function setBackground(index) {
       var url = images[index];
@@ -14,6 +41,9 @@
       background.removeClass('active');
       background.css('background-image', imgUrl);
       background.addClass('active');
+      background.data('background', index);
+      // console.log(background.data('background'));
+      resize();
     }
 
     function clearBackground() {
