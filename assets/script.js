901c047ad8289d0210e07166e9bcb7c756cd4bbc
liuyanghejerry <liuyanghejerry@126.com>
2015-01-03 21:31:07 +0800

    实现定时切换

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,17 @@
 (function() {
   $(document).ready(function(){
     var f = attachBackground();
-    attachLink(f.setBackground, f.clearBackground);
+
+    if($('html.index').length) {
+      attachLink(f.setBackground, f.clearBackground);
+    }
+
+    if($('html.info').length) {
+      setInterval(function(){f.nextBackground();}, 5000);
+    }
   });
 
   function attachBackground() {
-    var body = $('body');
     var backgrounds = $('.background-slider .background');
     var images = $('.background-slider .background img').map(function(index,item){return $(item).prop('src')});
 
@@ -24,9 +30,31 @@
       backgrounds.removeClass('active');
     }
 
+    function nextBackground() {
+      var item = $('.background-slider .background.active');
+      var cur = backgrounds.index(item);
+      var next = (cur+1) % backgrounds.length;
+      setBackground(next);
+      console.log(cur, next);
+    }
+
+    function previousBackground() {
+      var item = $('.background-slider .background.active');
+      var cur = backgrounds.index(item);
+      var previous = (cur+backgrounds.length-1) % backgrounds.length;
+      setBackground(previous);
+      console.log(cur, previous);
+    }
+
     setBackground(0);
 
-    return {setBackground: setBackground, clearBackground: clearBackground};
+    return {
+      setBackground: setBackground, 
+      clearBackground: clearBackground, 
+      nextBackground: nextBackground, 
+      previousBackground: previousBackground, 
+      backgorundCount: backgrounds.length
+    };
   }
 
   function attachLink(setBackground, clearBackground) {
