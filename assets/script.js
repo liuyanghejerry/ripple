f9309a6b5d043e64fae49f88f87050e39fb4c653
liuyanghejerry <liuyanghejerry@126.com>
2015-01-06 10:12:05 +0800

    增加projects雏形数据结构
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,23 @@
 (function() {
   $(document).ready(function(){
-    var f = attachBackground();
 
     if($('html.index').length) {
-      attachLink(f.setBackground, f.clearBackground);
+      var f = attachBackground();
+      attachMainLink(f.setBackground, f.clearBackground);
     }
 
     if($('html.info').length) {
+      var f = attachBackground();
       setInterval(function(){f.nextBackground();}, 5000);
+    }
+
+    if($('html.project').length) {
+      attachMenuLink();
+      loadProjects().done(function(data) {
+        setProjectContents(data, 'identity', 'co&co');
+      }).fail(function(err) {
+        console.log(err);
+      });
     }
   });
 
@@ -55,11 +65,38 @@
     };
   }
 
-  function attachLink(setBackground, clearBackground) {
+  function attachMainLink(setBackground, clearBackground) {
     var links = $('.main-content .main-link');
     links.each(function(index, link) {
       link = $(link);
       link.hover(function(){setBackground(index)});
+    });
+  }
+
+  function loadProjects() {
+    return $.getJSON("assets/project-contents.json", {});
+  }
+
+  function attachMenuLink() {
+    $('.header .nav li > a')
+    .hover(function(ev) {
+      $(this).parent().find('.inner-content').slideDown();
+    }, function() {
+      $(this).parent().not('.selected').find('.inner-content').slideUp();
+    })
+    .click(function(ev) {
+      //
+    });
+  }
+
+  function setProjectContents(data, main, sub) {
+    $('.background-slider .background').remove();
+    data[main][sub].backgrounds.forEach(function(item) {
+      var img = $('<img>').prop('src', item);
+      var background = $('<div>').addClass('background');
+      background.append(img);
+      $('.background-slider').append(background);4
+      attachBackground();
     });
   }
 
